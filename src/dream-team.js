b19ec15b84a7e1a91a4a736361c53f9770cfeb6f
Dzmitrysiniakou <jack.pott@mail.ru>
2020-03-22 20:47:36 +0300

    feat dream team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !members[0]) return false;
+  let output = [];
+  for(let i = 0; i < members.length; i++){
+    if (typeof members[i] == 'string'){
+      output.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  output.sort();
+  return output.reduce((first, second) => first + second);
 };
