1ecc8bf453c96316db3e2dc6b4673e37ffbec67e
Dzmitrysiniakou <jack.pott@mail.ru>
2020-09-16 17:35:40 +0300

    feat: extended-repeater tsk done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,18 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
+module.exports = function repeater(str, options) {
+    // throw 'Not implemented';
     // remove line with error and write your code here
+    let repStr = str;
+    let outputString = '';
+
+    for (let i = 0; i < options.additionRepeatTimes; i++){
+        repStr = repStr + options.addition + ' ';
+    }
+
+    repStr = repStr.trim().split(' ').join(options.additionSeparator);
+
+    for (let i = 0; i < options.repeatTimes; i++){
+        outputString = outputString + repStr + ' ';
+    }
+
+    return outputString.trim().split(' ').join(options.separator);
 };
-  
